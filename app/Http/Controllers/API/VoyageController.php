29e0cd84e75d9602c6d0cfa1d24a558b98642d80
Prskvs <29263845+Prskvs@users.noreply.github.com>
2022-01-17 16:17:54 +0200

    Voyages update with test.

--- app/Http/Controllers/API/VoyageController.php
+++ app/Http/Controllers/API/VoyageController.php
@@ -14,8 +14,8 @@ class VoyageController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'vessel_id' => 'required|int',
-            'start' => 'required|date_format:Y-m-d',
-            'end' => 'required|date_format:Y-m-d',
+            'start' => 'required|date_format:Y-m-d H:i:s',
+            'end' => 'required|date_format:Y-m-d H:i:s',
             'revenues' => 'required|numeric|between:0,99999999.99',
             'expenses' => 'required|numeric|between:0,99999999.99',
         ]);
@@ -28,13 +28,18 @@ class VoyageController extends Controller
             $vessel = Vessel::find($request->vessel_id);
             $code = $vessel->name . '-' . $request->start;
 
-            $vessel->voyages()->create([
+            $voyage = $vessel->voyages()->create([
                 'status' => 'pending',
                 'start' => $request->start,
                 'end' => $request->end,
                 'code' => $code,
                 'revenues' => $request->revenues,
                 'expenses' => $request->expenses,
+            ]);
+
+            return response()->json([
+                'success' => 'Voyage successfully stored!',
+                'voyage_id' => $voyage->id,
             ]);
         }
         catch (\Exception $e) {
@@ -43,8 +48,59 @@ class VoyageController extends Controller
             ]);
         }
 
-        return response()->json([
-            'message' => 'success',
+        // Fallback
+        return response()->json();
+    }
+
+    public function update(Voyage $voyage, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'start' => 'date_format:Y-m-d H:i:s',
+            'end' => 'date_format:Y-m-d H:i:s',
+            'status' => 'string',
+            'revenues' => 'numeric|between:0,99999999.99',
+            'expenses' => 'numeric|between:0,99999999.99',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        if ($request->status === 'ongoing') {
+            $ongoing_voyages = Voyage::where([
+                ['vessel_id', '=', $voyage->vessel_id],
+                ['status', $request->status],
+                ])->first();
+
+            if (!empty($ongoing_voyages)) {
+                return response()->json([
+                    'failure' => 'Vessel of this voyage has already an ongoing voyage!',
+                ]);
+            }
+        }
+
+        try {
+            // Optional update
+            $to_update = [];
+            foreach ($request as $key => $value) {
+                if ( in_array($key, ['start','end','status','revenues','expenses']) ) {
+                    $to_update[$key] = $value;
+                }
+            }
+
+            $voyage->update($to_update);
+
+            return response()->json([
+                'success' => 'Voyage successfully updated!',
+            ]);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ]);
+        }
+
+        // Fallback
+        return response()->json();
     }
 }
